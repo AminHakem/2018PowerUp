4d1c35985b151356e1a3142dc5cdf45a81cfb1fe
Meryem Esa <meresa14@gmail.com>
2017-01-10 19:33:20 -0800

    fill in TimeDrive

--- src/org/usfirst/frc/team3501/robot/commands/driving/TimeDrive.java
+++ src/org/usfirst/frc/team3501/robot/commands/driving/TimeDrive.java
@@ -2,41 +2,46 @@ package org.usfirst.frc.team3501.robot.commands.driving;
 
 import org.usfirst.frc.team3501.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class TimeDrive extends Command {
+    Timer timer;
+    double motorVal, time;
 
-    public TimeDrive() {
+    public TimeDrive(double time, double motorVal) {
         requires(Robot.getDriveTrain());
+
+        timer = new Timer();
+        this.time = time;
+        this.motorVal = motorVal;
     }
 
-    // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        timer.start();
+        Robot.getDriveTrain().setMotorValues(motorVal, motorVal);
     }
 
-    // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return timer.get() >= time;
     }
 
-    // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.getDriveTrain().stop();
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+        end();
     }
 }
