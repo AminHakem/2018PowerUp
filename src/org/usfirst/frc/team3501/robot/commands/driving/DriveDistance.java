aa160dfd8f61661dad049ff753e8547faae79b5c
Meryem Esa <meresa14@gmail.com>
2017-01-10 18:46:19 -0800

    add DriveDistance skeleton and TurnForAngle skeleton

---
+++ src/org/usfirst/frc/team3501/robot/commands/driving/DriveDistance.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team3501.robot.commands.driving;
+
+import org.usfirst.frc.team3501.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveDistance extends Command {
+
+    public DriveDistance() {
+        requires(Robot.getDriveTrain());
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+}
