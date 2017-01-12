1ff0da7b73c20fd9f292644e4fc99d04765300a7
shainachen <shaina.sierra@gmail.com>
2017-01-11 20:13:00 -0800

    Add javadoc comments for Shooter skeleton methods

--- src/org/usfirst/frc/team3501/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team3501/robot/subsystems/Shooter.java
@@ -5,26 +5,54 @@ public class Shooter {
 
 	}
 
+	/**
+	 * Stops fly wheel
+	 */
 	public void stopFlywheel() {
 
 	}
 
-	public void runFlywheel(double speed, double seconds) {
+	/**
+	 * Runs the fly wheel at a given speed in () for input time in seconds
+	 * 
+	 * @param speed
+	 *            in ()
+	 * @param time
+	 *            in seconds
+	 */
+	public void runFlywheel(double speed, double time) {
 
 	}
 
+	/**
+	 * Stops index wheel
+	 */
 	public void stopIndexWheel() {
 
 	}
 
-	public void runIndexWheel(double speed, double seconds) {
+	/**
+	 * Runs index wheel at a given speed in () for input time in seconds
+	 * 
+	 * @param speed
+	 *            in ()
+	 * @param time
+	 *            in seconds
+	 */
+	public void runIndexWheel(double speed, double time) {
 
 	}
 
+	/**
+	 * Runs fly wheel continuously until ________
+	 */
 	public void runFlywheelContinuous() {
 
 	}
 
+	/**
+	 * Runs index wheel continuously until ________
+	 */
 	public void runIndexWheelContinuous() {
 
 	}
