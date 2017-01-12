41dfad94fed57fdb1b545fa7ff1ca6f939885bbd
shainachen <shaina.sierra@gmail.com>
2017-01-11 19:59:46 -0800

    Add command skeletons for Shooter

--- src/org/usfirst/frc/team3501/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team3501/robot/subsystems/Shooter.java
@@ -1,5 +1,31 @@
 package org.usfirst.frc.team3501.robot.subsystems;
 
 public class Shooter {
+	public Shooter() {
 
+	}
+
+	public void stopFlywheel() {
+
+	}
+
+	public void runFlywheel(double speed, double seconds) {
+
+	}
+
+	public void stopIndexWheel() {
+
+	}
+
+	public void runIndexWheel(double speed, double seconds) {
+
+	}
+
+	public void runFlywheelContinuous() {
+
+	}
+
+	public void runIndexWheelContinuous() {
+
+	}
 }
