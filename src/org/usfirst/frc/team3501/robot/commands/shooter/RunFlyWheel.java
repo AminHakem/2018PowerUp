f21e65b401a96a45d0cec83b3443464bfb107c14
shainachen <shaina.sierra@gmail.com>
2017-01-11 20:39:02 -0800

    Create skeleton commands for shooter

--- src/org/usfirst/frc/team3501/robot/commands/shooter/RunFlyWheel.java
+++ src/org/usfirst/frc/team3501/robot/commands/shooter/RunFlyWheel.java
@@ -12,6 +12,27 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class RunFlyWheel extends Command {
 
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
