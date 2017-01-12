b06733de632ce2ee4f986f89617940757fee9718
Cindy Zhang <cindyzyx9@gmail.com>
2017-01-11 21:06:17 -0800

    fix port constants for joystick and drive train

--- src/org/usfirst/frc/team3501/robot/Constants.java
+++ src/org/usfirst/frc/team3501/robot/Constants.java
@@ -9,7 +9,7 @@ package org.usfirst.frc.team3501.robot;
 public class Constants {
   public static class OI {
     public final static int LEFT_STICK_PORT = 0;
-    public final static int RIGHT_STICK_PORT = 0;
+    public final static int RIGHT_STICK_PORT = 1;
   }
 
   public static class DriveTrain {
@@ -17,13 +17,13 @@ public class Constants {
     public static final int FRONT_LEFT = 1;
     public static final int FRONT_RIGHT = 3;
     public static final int REAR_LEFT = 2;
-    public static final int REAR_RIGHT = 9;
+    public static final int REAR_RIGHT = 4;
 
     // ENCODERS
-    public static final int ENCODER_LEFT_A = 2;
-    public static final int ENCODER_LEFT_B = 3;
-    public static final int ENCODER_RIGHT_A = 0;
-    public static final int ENCODER_RIGHT_B = 1;
+    public static final int ENCODER_LEFT_A = 0;
+    public static final int ENCODER_LEFT_B = 1;
+    public static final int ENCODER_RIGHT_A = 2;
+    public static final int ENCODER_RIGHT_B = 3;
 
     public static final int INCHES_PER_PULSE = 0;
   }
