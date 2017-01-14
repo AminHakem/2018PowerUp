c0860df7686073da86c6021170b168b5101e00a5
shainachen <shaina.sierra@gmail.com>
2017-01-14 12:04:42 -0800

    Edit JavaDoc comment, instantiate motorVal and time variable

--- src/org/usfirst/frc/team3501/robot/commands/shooter/RunIndexWheel.java
+++ src/org/usfirst/frc/team3501/robot/commands/shooter/RunIndexWheel.java
@@ -3,43 +3,47 @@ package org.usfirst.frc.team3501.robot.commands.shooter;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * Runs index wheel at a given speed in () for input time in seconds
+ * Runs index wheel at a given speed in () for given time in seconds
  *
  * @param speed
- *            in ()
+ *          in ()
  * @param time
- *            in seconds
+ *          in seconds
+ * @author shaina
  */
 public class RunIndexWheel extends Command {
-	public RunIndexWheel() {
+  private double time;
+  private double motorVal;
 
-	}
+  public RunIndexWheel(double motorVal, double time) {
+    this.motorVal = motorVal;
+    this.time = time;
+  }
 
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-	}
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
 
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {
-	}
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
 
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-	}
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {
-	}
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
 
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 
 }
