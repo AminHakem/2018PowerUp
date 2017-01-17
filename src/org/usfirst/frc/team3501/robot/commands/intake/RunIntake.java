763ccd44317c85b4e20b51250270606a25c1903a
Meeta <fasmeeta@gmail.com>
2017-01-16 20:02:09 -0800

    create skeleton code for intake commands

---
+++ src/org/usfirst/frc/team3501/robot/commands/intake/RunIntake.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team3501.robot.commands.intake;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * Starts running the intake for a specific period of time that the user inputs.
+ * 
+ * @author Meeta
+ */
+public class RunIntake extends Command {
+  public RunIntake(double timeToMove) {
+
+  }
+
+  @Override
+  protected boolean isFinished() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
