0ceab9a4ab7dec9aa01508a922351456f17679c3
Meeta <fasmeeta@gmail.com>
2017-01-17 19:16:24 -0800

    add intake subsystem class

---
+++ src/org/usfirst/frc/team3501/robot/subsystems/Intake.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team3501.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/***
+ *
+ *
+ * @author Meeta
+ *
+ */
+public class Intake extends Subsystem {
+  public Intake() {
+
+  }
+
+  /**
+   * Runs the intake continuously
+   */
+  public void RunContinous() {
+
+  }
+
+  /**
+   * Starts running the intake for a specific period of time that the user
+   * inputs.
+   *
+   * @param timeToMove
+   *          in seconds
+   */
+  public void RunIntake(double timeToMove) {
+
+  }
+
+  /**
+   * Stops the intake
+   */
+  public void StopIntake() {
+
+  }
+
+  @Override
+  protected void initDefaultCommand() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
