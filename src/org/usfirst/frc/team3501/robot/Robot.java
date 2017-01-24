6f3b6d29952ac5369df900e2987d77373c21edb8
Arunima DIvya <adivya822@student.fuhsd.org>
2017-01-23 19:11:57 -0800

    Add intake instance and getter method for intake

--- src/org/usfirst/frc/team3501/robot/Robot.java
+++ src/org/usfirst/frc/team3501/robot/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3501.robot;
 
 import org.usfirst.frc.team3501.robot.commands.driving.TimeDrive;
 import org.usfirst.frc.team3501.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team3501.robot.subsystems.Intake;
 import org.usfirst.frc.team3501.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -11,13 +12,14 @@ public class Robot extends IterativeRobot {
   private static DriveTrain driveTrain;
   private static Shooter shooter;
   private static OI oi;
+  private static Intake intake;
 
   @Override
   public void robotInit() {
     driveTrain = DriveTrain.getDriveTrain();
     oi = OI.getOI();
     shooter = Shooter.getShooter();
-
+    intake = Intake.getIntake();
   }
 
   public static DriveTrain getDriveTrain() {
@@ -30,6 +32,10 @@ public class Robot extends IterativeRobot {
 
   public static OI getOI() {
     return OI.getOI();
+  }
+
+  public static Intake getIntake() {
+    return Intake.getIntake()
   }
 
   @Override
