5690f62930035364251465cc6af1e3bc79993bf9
Arunima DIvya <adivya822@student.fuhsd.org>
2017-01-25 20:03:43 -0800

    fill in code

--- src/org/usfirst/frc/team3501/robot/commands/intake/ReverseIntakeContinuous.java
+++ src/org/usfirst/frc/team3501/robot/commands/intake/ReverseIntakeContinuous.java
@@ -18,11 +18,14 @@ public class ReverseIntakeContinuous extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    Robot.getIntake().runReverseIntake();
+
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -34,11 +37,13 @@ public class ReverseIntakeContinuous extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.getIntake().stopIntake();
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
