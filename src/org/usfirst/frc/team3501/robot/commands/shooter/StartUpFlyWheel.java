5ba0f0183b1719b28df181abea39839085c521bb
Christopher Zhu <christopherzhu@Christophers-MacBook-Pro.local>
2017-02-02 21:07:45 -0800

    add variables

--- src/org/usfirst/frc/team3501/robot/commands/shooter/StartUpFlyWheel.java
+++ src/org/usfirst/frc/team3501/robot/commands/shooter/StartUpFlyWheel.java
@@ -1,36 +1,49 @@
 package org.usfirst.frc.team3501.robot.commands.shooter;
 
+import org.usfirst.frc.team3501.robot.Robot;
+import org.usfirst.frc.team3501.robot.subsystems.Shooter;
+import org.usfirst.frc.team3501.robot.utils.PIDController;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class StartUpFlyWheel extends Command {
+  private Shooter shooter = Robot.getShooter();
+  private PIDController wheelController;
+  private double wheelP;
+  private double wheelI;
+  private double wheelD;
+  private double motorVal;
+  private double target;
+  
+  public StartUpFlyWheel() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(shooter);
+  }
 
-    public StartUpFlyWheel() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+  // Called just before this Command runs the first time
+  protected void initialize() {
+    
+  }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+  // Called repeatedly when this Command is scheduled to run
+  protected void execute() {
+  }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+  // Make this return true when this Command no longer needs to run execute()
+  protected boolean isFinished() {
+    return false;
+  }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+  // Called once after isFinished returns true
+  protected void end() {
+  }
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  protected void interrupted() {
+  }
 }
