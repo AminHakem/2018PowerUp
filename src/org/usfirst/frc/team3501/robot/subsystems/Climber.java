9c27204345d7507e82badee2007193a5b816db16
niyatisriram <niyatisriram@gmail.com>
2018-01-20 15:29:01 -0800

    change ramp motor from piston to servo

--- src/org/usfirst/frc/team3501/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team3501/robot/subsystems/Climber.java
@@ -2,31 +2,35 @@ package org.usfirst.frc.team3501.robot.subsystems;
 
 import org.usfirst.frc.team3501.robot.Constants;
 import org.usfirst.frc.team3501.robot.MathLib;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.Solenoid;
+
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * @author: niyatisriram Climber with motors that drive winch to move robot up to climb Ramp that
+ * @author: niyatisriram Climber with motors that drive winch to move robot up
+ *          to climb Ramp that
  *          lifts other robots
  */
 public class Climber extends Subsystem {
   private static Climber climber;
   private static WPI_TalonSRX rightWinch;
   private static WPI_TalonSRX leftWinch;
-  private static Solenoid rampPiston;
+  private static Servo rampPiston;
 
   private static boolean on = true, off = false;
 
   public static final int WINCH_SPEED = 0;
+  private static final double SERVO_ANGLE = 0;
   public boolean inJoystickClimb = false;
 
   private Climber() {
     // MOTOR CONTROLLER
     rightWinch = new WPI_TalonSRX(Constants.Climber.RIGHT_WINCH);
     leftWinch = new WPI_TalonSRX(Constants.Climber.LEFT_WINCH);
-    rampPiston = new Solenoid(Constants.Climber.PISTON_MODULE, Constants.Climber.PISTON_CHANNEL);
+    rampPiston = new Servo(Constants.Climber.PISTON_MODULE);
   }
 
   public static Climber getClimber() {
@@ -57,11 +61,11 @@ public class Climber extends Subsystem {
   }
 
   public static void extendPiston() {
-    rampPiston.set(on);
+    rampPiston.set(SERVO_ANGLE);
   }
 
   public static void retractPiston() {
-    rampPiston.set(off);
+    rampPiston.set(0);
   }
 
   public static void stop() {
@@ -70,5 +74,6 @@ public class Climber extends Subsystem {
   }
 
   @Override
-  public void initDefaultCommand() {}
+  public void initDefaultCommand() {
+  }
 }
