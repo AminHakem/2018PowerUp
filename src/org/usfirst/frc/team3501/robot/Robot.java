11d0e5136934dd080c2c4ea021f4e2b1dd3ceff2
Cindy Zhang <cindyzyx9@gmail.com>
2018-01-22 20:23:35 -0800

    import correct climber subsystem

--- src/org/usfirst/frc/team3501/robot/Robot.java
+++ src/org/usfirst/frc/team3501/robot/Robot.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team3501.robot;
 
-import org.usfirst.frc.team3501.robot.Constants.Climber;
+import org.usfirst.frc.team3501.robot.subsystems.Climber;
 import org.usfirst.frc.team3501.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team3501.robot.subsystems.Elevator;
 import org.usfirst.frc.team3501.robot.subsystems.Intake;
