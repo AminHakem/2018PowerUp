3a994495190842a2ac23f4a95fb5eca6c293f706
Rahul Mannikar <rahulmannikar@gmail.com>
2018-02-17 11:26:48 -0800

    Changed alignWithCube to use boxSize instead of boxY

--- src/org/usfirst/frc/team3501/robot/commands/driving/AlignWithCube.java
+++ src/org/usfirst/frc/team3501/robot/commands/driving/AlignWithCube.java
@@ -7,65 +7,66 @@ import org.usfirst.frc.team3501.robot.utils.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * Command starts a thread which will get values from RaspberryPi regarding the
- * alignment of a cube using camera input Is currently mapped to the X button on
- * controller. Uses values from camera to align itself with the cube by moving
- * horizontally
- * 
+ * Command starts a thread which will get values from RaspberryPi regarding the alignment of a cube
+ * using camera input Is currently mapped to the X button on controller. Uses values from camera to
+ * align itself with the cube by moving horizontally
+ *
  * @author Amin Hakem
  * @author Alexander Ivanov (a bit of help later on;)
  *
  */
 public class AlignWithCube extends Command {
 
-	private PIDController alignmentControllerX;
-	private PIDController alignmentControllerY;
-	private NetworkThread thread;
+  private PIDController alignmentControllerX;
+  private PIDController alignmentControllerY;
+  private NetworkThread thread;
 
-	public AlignWithCube() {
-		requires(Robot.getDriveTrain());
-		alignmentControllerX = new PIDController(DriveTrain.driveSidewaysP, DriveTrain.driveSidewaysI, 0);
-		alignmentControllerX.setDoneRange(3);
-	
-		alignmentControllerY = new PIDController(DriveTrain.driveStraightP, DriveTrain.driveStraightI, 0);
-        alignmentControllerY.setDoneRange(3);
-         
-		// initialize a thread which will run code to constantly update
-		thread = new NetworkThread();
-		thread.start();
-        System.out.println("AlignWithCube initialized, thread started");
-		// RaspberryPi will output 0 when camera is aligned
-	}
+  public AlignWithCube() {
+    requires(Robot.getDriveTrain());
+    alignmentControllerX = new PIDController(DriveTrain.driveSidewaysP,
+        DriveTrain.driveSidewaysI, 0);
+    alignmentControllerX.setDoneRange(3);
 
-	@Override
-	protected void initialize() {
-		alignmentControllerX.setSetPoint(0);
-	      alignmentControllerY.setSetPoint(70);
-	        System.out.println("setpoints set");
+    alignmentControllerY = new PIDController(DriveTrain.driveStraightP,
+        DriveTrain.driveStraightI, 0);
+    alignmentControllerY.setDoneRange(0.2);
 
-	}
+    // initialize a thread which will run code to constantly update
+    thread = new NetworkThread();
+    thread.start();
+    System.out.println("AlignWithCube initialized, thread started");
+    // RaspberryPi will output 0 when camera is aligned
+  }
 
-	@Override
-	protected void execute() {
+  @Override
+  protected void initialize() {
+    alignmentControllerX.setSetPoint(0);
+    alignmentControllerY.setSetPoint(3);
+    System.out.println("setpoints set");
 
-		if (NetworkThread.isBoxVisible()) {
-			double outputX = alignmentControllerX.calcPID(NetworkThread.getBoxX());
-			double outputY = alignmentControllerX.calcPID(NetworkThread.getBoxY());
-            DriveTrain.getDriveTrain().mecanumDrive(-outputX,-outputY, 0);
-		}
-	}
+  }
 
-	@Override
-	protected boolean isFinished() {
-		return this.alignmentControllerX.isDone()&&this.alignmentControllerY.isDone();
-	}
+  @Override
+  protected void execute() {
 
-	@Override
-	protected void end() {
-      System.out.println("AlignWithCube finished");
-	}
+    if (NetworkThread.isBoxVisible()) {
+      double outputX = alignmentControllerX.calcPID(NetworkThread.getBoxX());
+      double outputY = alignmentControllerX.calcPID(NetworkThread.getBoxSize());
+      DriveTrain.getDriveTrain().mecanumDrive(-outputX, -outputY, 0);
+    }
+  }
 
-	@Override
-	protected void interrupted() {
-	}
+  @Override
+  protected boolean isFinished() {
+    return this.alignmentControllerX.isDone()
+        && this.alignmentControllerY.isDone();
+  }
+
+  @Override
+  protected void end() {
+    System.out.println("AlignWithCube finished");
+  }
+
+  @Override
+  protected void interrupted() {}
 }
